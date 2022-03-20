41ad16c51c2e1076574cb1d95b2c89f0118e60b5
Aliaksandr Klimovich <aliaksandr_klimovich@icloud.com>
2022-03-20 20:15:26 +0200

    add basic test

---
+++ test.py
@@ -0,0 +1,13 @@
+from main import TestCase, TestRunner
+
+
+class MyTestCase(TestCase):
+    def run(self):
+        result = self.assert_equals(True, True)
+        print('assertion result:', result)
+
+
+if __name__ == '__main__':
+    runner = TestRunner()
+    runner.add_test_cases(MyTestCase)
+    runner.run()

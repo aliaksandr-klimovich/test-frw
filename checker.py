d35e3c60a1d9645bd5e116af67dad6ef7b453a89
Aliaksandr Klimovich <aliaksandr_klimovich@icloud.com>
2022-05-22 11:57:22 +0300

    remove MetaChecker, implement separate methods in Checker class

--- checker.py
+++ checker.py
@@ -1,103 +1,101 @@
-from exception import AssertionFail
-from result import TestVerdict, CHECK_TEMPLATE
+from exception import AssertionFail, TestFrwException
+from log import log
+from result import TestVerdict
 
 
-class MetaChecker(type):
-    """
-    To decorate check_ methods of Checker class.
-    The main purpose of the wrapper is to pass the check_ result to TestResult instance.
-
-    It also creates (generates) similar assert_ methods in Checker class.
-    """
-
-    def __new__(mcs, name, bases, namespace):
-        if name == 'Checker':
-            ns_checks = {}
-            ns_asserts = {}
-            check_prefix = 'check_'
-            assert_prefix = 'assert_'
-            for k, v in namespace.items():
-                if callable(v) and k.startswith(check_prefix):
-                    check_method = mcs._wrap_check(v)
-                    ns_checks[k] = check_method
-                    assert_method_name = assert_prefix + k[len(check_prefix):]
-                    assert_method = mcs._wrap_check2(check_method)
-                    ns_asserts[assert_method_name] = assert_method
-            namespace.update(ns_checks)
-            namespace.update(ns_asserts)
-        return type.__new__(mcs, name, bases, namespace)
-
-    def _wrap_check(check_method):
-        """Wraps check_ methods of the Checker class."""
-        def check_wrapper(self, *args, message='', **kwargs):
-            check_results = dict.fromkeys(CHECK_TEMPLATE)
-            check_results['args'] = args
-            check_results['kwargs'] = kwargs
-            check_results['message'] = message
-            self._test_result.checks.append(check_results)
-            try:
-                check_result = check_method(self, *args, **kwargs)
-            except:
-                # todo:
-                #  What to do in case the objects cannot be compared and this method raises an Exception?
-                #  Need to handle this exception.
-                raise
-            check_results['result'] = check_result
-            if check_result is True:
-                self._test_result.update_verdict(TestVerdict.PASSED)
-            elif check_result is False:
-                self._test_result.update_verdict(TestVerdict.FAILED)
-            else:
-                # todo: Handle invalid check_result.
-                pass
-            return check_result
-        return check_wrapper
-
-    def _wrap_check2(check_method):
-        """Wraps _wrap_check methods to produce assert_ methods."""
-        def assert_method(*args, **kwargs):
-            res = check_method(*args, **kwargs)
-            if not res:
-                raise AssertionFail()  # todo: Any args/ kwargs to provide to the error instance?
-        return assert_method
-
-
-class Checker(metaclass=MetaChecker):
+class Checker:
     """
     Mixin for TestCase. It contains all necessary methods to check and assert entities.
     check_ method do not fail test case, while the assert_ methods do raise an exception.
     """
 
-    def check_true(self, measured, message=''):
-        return measured is True
+    # def check_true(self, measured, message=''):
+    #     return measured is True
+    #
+    # def assert_true(self, measured, message=''):
+    #     """stub"""
+    #
+    # def check_false(self, measured, message=''):
+    #     return measured is False
+    #
+    # def assert_false(self, measured, message=''):
+    #     """stud"""
+    #
+    # def check_is(self, measured, expected, message=''):
+    #     return measured is expected
+    #
+    # def assert_is(self, measured, expected, message=''):
+    #     """stub"""
 
-    def assert_true(self, measured, message=''):
-        """stub"""
+    @staticmethod
+    def _log_check_setup(actual, expected, message):
+        log.info(message)
+        log.info(f'actual: {actual}')
+        log.info(f'expected: {expected}')
 
-    def check_false(self, measured, message=''):
-        return measured is False
+    @staticmethod
+    def _log_verdict(verdict):
+        log.info(f'verdict: {verdict.name}')
 
-    def assert_false(self, measured, message=''):
-        """stud"""
+    @staticmethod
+    def _log_cannot_compare():
+        log.info('objects cannot be compared: comparison result is not a bool value')
 
-    def check_is(self, measured, expected, message=''):
-        return measured is expected
+    @staticmethod
+    def _log_comparison_error():
+        log.info('objects cannot be compared: error is raised while comparing objects')
 
-    def assert_is(self, measured, expected, message=''):
-        """stub"""
+    def _compare_with_try_2(self, actual, sign, expected, ):
+        try:
+            if sign == '==':
+                result = actual == expected
+            elif sign == '!=':
+                result = actual != expected
+            elif sign == 'is':
+                result = actual is expected
+            else:
+                raise TestFrwException(f'Invalid sign: "{sign}".')
+        except:
+            # error is raised during comparison
+            # e.g. objects cannot be compared
+            self._log_comparison_error()
+            result = False
+            # todo: update verdict?
+        return result
 
-    def check_eq(self, measured, expected, message=''):
-        return measured == expected
+    def _update_verdict(self, result: bool) -> bool:
+        if result is True:
+            verdict = TestVerdict.PASSED
+            self._log_verdict(verdict)
+            self._test_result.update_verdict(verdict)
+        elif result is False:
+            verdict = TestVerdict.FAILED
+            self._log_verdict(verdict)
+            self._test_result.update_verdict(verdict)
+        else:
+            # result is not a bool value
+            self._log_cannot_compare()
+            result = False
+            # todo: update verdict?
+        return result
 
-    def assert_eq(self, measured, expected, message=''):
-        """stub"""
+    def check_eq(self, actual, expected, message=''):
+        self._log_check_setup(actual, expected, message)
+        result = self._compare_with_try_2(actual, '==', expected)
+        result = self._update_verdict(result)
+        return result
 
-    def fail(self, message=''):
-        check_results = dict.fromkeys(CHECK_TEMPLATE)
-        check_results['args'] = ()
-        check_results['kwargs'] = {}
-        check_results['message'] = message
-        check_results['result'] = False
-        self._test_result.checks.append(check_results)
-        self._test_result.update_verdict(TestVerdict.FAILED)
-        raise AssertionFail()
+    def assert_eq(self, actual, expected, message=''):
+        if self.check_eq(actual, expected, message) is False:
+            raise AssertionFail()
+        return True
+
+    # def fail(self, message=''):
+    #     check_results = dict.fromkeys(CHECK_TEMPLATE)
+    #     check_results['args'] = ()
+    #     check_results['kwargs'] = {}
+    #     check_results['message'] = message
+    #     check_results['result'] = False
+    #     self._test_result.checks.append(check_results)
+    #     self._test_result.update_verdict(TestVerdict.FAILED)
+    #     raise AssertionFail()
